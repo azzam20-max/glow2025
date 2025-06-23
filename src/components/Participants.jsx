2431bf31fefa4cd021d5081e33d5635257814ff6
sha_azzam <63727742+azzam20-max@users.noreply.github.com>
2025-06-23 15:56:56 +0700

    nambah preview participant
    nambah participant

--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import './Participants.css';
+import React from "react";
+import "./Participants.css";
 
 function Participants() {
-  // Dummy data peserta
-  const participants = [
-    { name: 'Ayu Lestari', country: 'Indonesia' },
-    { name: 'Muhammad Farhan', country: 'Malaysia' },
-    { name: 'Nurul Aini', country: 'Singapore' },
-    { name: 'Somchai Prasert', country: 'Thailand' },
-    { name: 'Jose Rizal', country: 'Philippines' },
-    { name: 'Nguyen Thi Mai', country: 'Vietnam' }
-  ];
-
   return (
     <section className="participants-section">
       <div className="participants-container">
-        <h1>List of Participants</h1>
-        <ul className="participants-list">
-          {participants.map((p, index) => (
-            <li key={index} className="participant-item">
-              <span className="participant-name">{p.name}</span>
-              <span className="participant-country">{p.country}</span>
-            </li>
-          ))}
-        </ul>
+        <h2 className="participants-heading">List of Participants</h2>
+        <p className="participants-subtext">
+          Below is the live participant list embedded from Google Sheets.
+        </p>
+
+        <div className="spreadsheet-wrapper">
+          <iframe
+            src="https://docs.google.com/spreadsheets/d/1uwhJ-vsKm1hUZv6llNVXdl6mILl3y04g/edit?usp=sharing&ouid=105908272783749210970&rtpof=true&sd=true/pubhtml?widget=true&headers=false"
+            title="Participants Spreadsheet"
+            width="100%"
+            height="900"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <div className="spreadsheet-button">
+          <a
+            href="https://docs.google.com/spreadsheets/d/1uwhJ-vsKm1hUZv6llNVXdl6mILl3y04g/edit?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="open-btn"
+          >
+            Open Spreadsheet
+          </a>
+        </div>
       </div>
     </section>
   );
