36b7d24277b42a448f4f0eb0710373c47761a499
sha_azzam <63727742+azzam20-max@users.noreply.github.com>
2025-06-25 16:57:10 +0700

    push

--- src/components/Participants.jsx
+++ src/components/Participants.jsx
@@ -9,17 +9,16 @@ function Participants() {
         <p className="participants-subtext">
           Open the spreadsheet below to view the list of participants for the event.
         </p>
-
-        {/* <div className="spreadsheet-wrapper">
+        <div className="spreadsheet-wrapper">
           <iframe
-            src="https://telkomuniversityofficial-my.sharepoint.com/:x:/g/personal/icao_telkomuniversity_ac_id/EVbtriVsiLhDlj9kcjCUyZQBUj4oYt7i7ndYdlQ5qxNTsw?e=8yZ3q6"
+            src="https://telkomuniversityofficial-my.sharepoint.com/personal/icao_telkomuniversity_ac_id/_layouts/15/Doc.aspx?sourcedoc={25aeed56-886c-43b8-963f-64723094c994}&action=embedview&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"
             title="Participants Spreadsheet"
-            width="100%"
-            height="900"
+            width="600px"
+            height="900px"
             frameBorder="0"
             allowFullScreen
           ></iframe>
-        </div> */}
+        </div>
 
         <div className="spreadsheet-button">
           <a
@@ -28,7 +27,7 @@ function Participants() {
             rel="noopener noreferrer"
             className="open-btn"
           >
-            Open Spreadsheet
+            Open Excel Web
           </a>
         </div>
       </div>
